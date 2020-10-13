1f199301c186b8a70f1c74b8c0079e6a2251e250
Fajar22isworo <fjrisworo@gmail.com>
2020-10-13 15:17:24 +0700

    Project saya

---
+++ ClassAnak/KaryawanHarian.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TugasLab8.ClassAnak
+{
+    public  class KaryawanHarian : Karyawan
+    {
+        public double UpahPerJam { get; set; }
+        public double JumlahJamKerja { get; set; }
+
+        public override double Gaji()
+        {
+            double Gaji = 0;
+            Gaji = UpahPerJam * JumlahJamKerja;
+            return Gaji;
+        }
+
+    }
+}
